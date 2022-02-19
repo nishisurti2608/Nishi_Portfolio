45ed3f2144ca1c1ffb779db54f8a7ddd53f07724
nishi <nishisurti2608@github.com>
2022-02-19 11:51:12 -0500

    Closing menu on each navigation link

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,29 +1,44 @@
+/**
+ * !functionality
+ * ?explanation to terms
+ */
+
+
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const nav_menu= document.getElementById('nav-menu'),
       nav_toggle = document.getElementById('nav-toggle'),
       nav_close= document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
+/* Validate if constant nav_toggle exists */
+/* classList: It is a read-only property of an element that returns a live collection of CSS classes * */
 
 if(nav_toggle){
-    nav_toggle.addEventListener('click', () => {
-        nav_menu.classList.add('show-menu')
+    nav_toggle.addEventListener('click', () => { 
+        nav_menu.classList.add('show-menu') //? will add class showmenu
     })
 }
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+/* Validate if constant nav_close exists */
 
 if(nav_close){
     nav_close.addEventListener('click',()=>
-    nav_menu.classList.remove('show-menu'))
+    nav_menu.classList.remove('show-menu')) //? will remove class showmenu
 }
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
 
+const navLink= document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu') //? will remove show-menu class when we click on each nav__link
+}
+
+navLink.forEach(n=> n.addEventListener('click', linkAction))
 /*==================== ACCORDION SKILLS ====================*/
 
 
